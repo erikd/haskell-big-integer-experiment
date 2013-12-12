59d5bd1ca588dd2e73d6838193be77881840c4e1
Erik de Castro Lopo <erikd@mega-nerd.com>
2013-12-12 21:47:46 +1100

    test-integer.hs : Switch to QuickCheck to compare two libs.

--- test-integer.hs
+++ test-integer.hs
@@ -2,11 +2,29 @@
 
 import Prelude hiding (Integer)
 
+import qualified Test.QuickCheck as QC
+
 import qualified GMP.Integer as G
 import qualified Simple.Integer as S
 
 main :: IO ()
 main = do
-    print $ G.mkInteger True [2, 1, 1, 1, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6]
-    print $ S.mkInteger True [2, 1, 1, 1, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6]
+    QC.quickCheck prop_same_mkInteger
+    QC.quickCheck prop_same_sum
 
+
+prop_same_mkInteger :: [Int] -> Bool
+prop_same_mkInteger lst =
+    let pos = concat . replicate 10 $ map abs lst
+    in show (G.mkInteger True pos) == show (S.mkInteger True pos)
+
+
+prop_same_sum :: [Int] -> [Int] -> Bool
+prop_same_sum a b =
+    let ap = concat . replicate 7 $ map abs a
+        bp = concat . replicate 7 $ map abs b
+        ga = G.mkInteger True ap
+        gb = G.mkInteger True bp
+        sa = S.mkInteger True ap
+        sb = S.mkInteger True bp
+    in show (ga + gb) == show (sa + sb)
