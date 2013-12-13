f3b2cd9a492a773364ff00a274621c80829bc494
Erik de Castro Lopo <erikd@mega-nerd.com>
2013-12-13 18:39:22 +1100

    test-integer.hs : Switch to hspec.

--- test-integer.hs
+++ test-integer.hs
@@ -2,29 +2,27 @@
 
 import Prelude hiding (Integer)
 
-import qualified Test.QuickCheck as QC
+import Test.Hspec
+import Test.Hspec.QuickCheck
 
 import qualified GMP.Integer as G
 import qualified Simple.Integer as S
 
 main :: IO ()
-main = do
-    QC.quickCheck prop_same_mkInteger
-    QC.quickCheck prop_same_sum
+main = hspec $
+    describe "Comparing GMP and Simple Integer operations:" testInteger
 
 
-prop_same_mkInteger :: [Int] -> Bool
-prop_same_mkInteger lst =
-    let pos = concat . replicate 10 $ map abs lst
-    in show (G.mkInteger True pos) == show (S.mkInteger True pos)
-
-
-prop_same_sum :: [Int] -> [Int] -> Bool
-prop_same_sum a b =
-    let ap = concat . replicate 7 $ map abs a
-        bp = concat . replicate 7 $ map abs b
-        ga = G.mkInteger True ap
-        gb = G.mkInteger True bp
-        sa = S.mkInteger True ap
-        sb = S.mkInteger True bp
-    in show (ga + gb) == show (sa + sb)
+testInteger :: Spec
+testInteger = do
+    prop "Can create Integers." $ \ lst ->
+        let pos = concat . replicate 10 $ map abs lst
+        in show (G.mkInteger True pos) == show (S.mkInteger True pos)
+    prop "Can add Integers." $ \ a b ->
+        let ap = concat . replicate 7 $ map abs a
+            bp = concat . replicate 7 $ map abs b
+            ga = G.mkInteger True ap
+            gb = G.mkInteger True bp
+            sa = S.mkInteger True ap
+            sb = S.mkInteger True bp
+        in show (ga + gb) == show (sa + sb)
