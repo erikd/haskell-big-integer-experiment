f0d4f93203afd1db21439c8f42233cb7efdadf55
Erik de Castro Lopo <erikd@mega-nerd.com>
2013-12-13 19:17:11 +1100

    test-integer.hs : Add subtract and multiply tests.

--- test-integer.hs
+++ test-integer.hs
@@ -16,13 +16,21 @@ main = hspec $
 testInteger :: Spec
 testInteger = do
     prop "Can create Integers." $ \ lst ->
-        let pos = concat . replicate 10 $ map abs lst
+        let pos = concat . replicate 8 $ map abs lst
         in show (G.mkInteger True pos) == show (S.mkInteger True pos)
     prop "Can add Integers." $ \ a b ->
-        let ap = concat . replicate 7 $ map abs a
-            bp = concat . replicate 7 $ map abs b
-            ga = G.mkInteger True ap
-            gb = G.mkInteger True bp
-            sa = S.mkInteger True ap
-            sb = S.mkInteger True bp
+        let (ga, gb, sa, sb) = mkFourIntegers 6 a b
         in show (ga + gb) == show (sa + sb)
+    prop "Can subtract Integers." $ \ a b ->
+        let (ga, gb, sa, sb) = mkFourIntegers 6 a b
+        in show (ga - gb) == show (sa - sb)
+    prop "Can multiply Integers." $ \ a b ->
+        let (ga, gb, sa, sb) = mkFourIntegers 4 a b
+        in show (ga * gb) == show (sa * sb)
+
+
+mkFourIntegers :: Int -> [Int] -> [Int] -> (G.Integer, G.Integer, S.Integer, S.Integer)
+mkFourIntegers rep a b =
+    let ap = concat . replicate rep $ map abs a
+        bp = concat . replicate rep $ map abs b
+    in (G.mkInteger True ap, G.mkInteger True bp, S.mkInteger True ap, S.mkInteger True bp)
