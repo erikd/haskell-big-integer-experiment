f5027400a06c9f5d937447cba5fd1c81a2823218
Erik de Castro Lopo <erikd@mega-nerd.com>
2013-12-19 21:53:02 +1100

    Add file New/GHC/Integer/Array.hs.

---
+++ New/GHC/Integer/Array.hs
@@ -0,0 +1,53 @@
+module New.GHC.Integer.Array where
+
+import Control.Monad.Primitive
+import Data.Primitive
+
+
+import New.GHC.Integer.Prim
+
+
+newtype MutableWordArray m = MWA (MutableByteArray (PrimState m))
+newtype MutableHalfWordArray m = MHWA (MutableByteArray (PrimState m))
+
+
+newWordArray :: PrimMonad m => Int -> m (MutableWordArray m)
+newWordArray len = do
+	marr <- newByteArray (len * sizeOf (0 :: FullWord))
+	return $ MWA marr
+
+newHalfWordArray :: PrimMonad m => Int -> m (MutableHalfWordArray m)
+newHalfWordArray len = do
+	marr <- newByteArray (len * sizeOf (0 :: HalfWord))
+	return $ MHWA marr
+
+newHalfWordArrayCleared :: PrimMonad m => Int -> m (MutableHalfWordArray m)
+newHalfWordArrayCleared len = do
+	marr <- newByteArray (len * sizeOf (0 :: HalfWord))
+	setByteArray marr 0 len (0 :: HalfWord)
+	return $ MHWA marr
+
+unsafeFreezeWordArray :: PrimMonad m => MutableWordArray m -> m ByteArray
+unsafeFreezeWordArray (MWA marr) = unsafeFreezeByteArray marr
+
+unsafeFreezeHalfWordArray :: PrimMonad m => MutableHalfWordArray m -> m ByteArray
+unsafeFreezeHalfWordArray (MHWA marr) = unsafeFreezeByteArray marr
+
+indexWordArray :: ByteArray -> Int -> FullWord
+indexWordArray = indexByteArray
+
+indexHalfWordArray :: ByteArray -> Int -> HalfWord
+indexHalfWordArray = indexByteArray
+
+
+indexWordArrayM :: Monad m => ByteArray -> Int -> m FullWord
+indexWordArrayM arr i = return $ indexByteArray arr i
+
+indexHalfWordArrayM :: Monad m => ByteArray -> Int -> m HalfWord
+indexHalfWordArrayM arr i = return $ indexByteArray arr i
+
+writeWordArray :: PrimMonad m => MutableWordArray m -> Int -> FullWord -> m ()
+writeWordArray (MWA marr) = writeByteArray marr
+
+writeHalfWordArray :: PrimMonad m => MutableHalfWordArray m -> Int -> HalfWord -> m ()
+writeHalfWordArray (MHWA marr) = writeByteArray marr
