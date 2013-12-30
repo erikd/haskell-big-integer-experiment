1d250161179c4ea1ea9c0251f6876820b0586ad8
Erik de Castro Lopo <erikd@mega-nerd.com>
2013-12-30 17:12:39 +1100

    Array : Add setWordArray and copyWordArray.

--- New/GHC/Integer/Array.hs
+++ New/GHC/Integer/Array.hs
@@ -13,28 +13,36 @@ newtype MutableHalfWordArray m = MHWA (MutableByteArray (PrimState m))
 
 newWordArray :: PrimMonad m => Int -> m (MutableWordArray m)
 newWordArray !len = do
-    marr <- newByteArray (len * sizeOf (0 :: FullWord))
+    !marr <- newByteArray (len * sizeOf (0 :: FullWord))
     return $ MWA marr
 
 newHalfWordArray :: PrimMonad m => Int -> m (MutableHalfWordArray m)
 newHalfWordArray !len = do
-    marr <- newByteArray (len * sizeOf (0 :: HalfWord))
+    !marr <- newByteArray (len * sizeOf (0 :: HalfWord))
     return $ MHWA marr
 
 newHalfWordArrayCleared :: PrimMonad m => Int -> m (MutableHalfWordArray m)
 newHalfWordArrayCleared !len = do
-    marr <- newByteArray (len * sizeOf (0 :: HalfWord))
+    !marr <- newByteArray (len * sizeOf (0 :: HalfWord))
     setByteArray marr 0 len (0 :: HalfWord)
     return $ MHWA marr
 
+-- | newPlaceholderWordArray : Create a place holder ByteArray for timesInteger
+-- where a zero length ByteArray is needed. Memory is actually allocated, but
+-- nothing is written to it os it will actually contain junk data.
+newPlaceholderWordArray :: PrimMonad m => m ByteArray
+newPlaceholderWordArray = do
+    !marr <- newByteArray (sizeOf (0 :: FullWord))
+    unsafeFreezeByteArray marr
 
 cloneHalfWordArrayExtend :: PrimMonad m=> Int -> ByteArray -> Int -> m (MutableHalfWordArray m)
 cloneHalfWordArrayExtend !oldLen !arr !newLen = do
-    marr <- newByteArray (newLen * sizeOf (0 :: HalfWord))
-    copyByteArray marr 0 arr 0 ((min oldLen newLen) * sizeOf (0 :: HalfWord))
+    !marr <- newByteArray (newLen * sizeOf (0 :: HalfWord))
+    if oldLen > 0
+        then copyByteArray marr 0 arr 0 ((min oldLen newLen) * sizeOf (0 :: HalfWord))
+        else return ()
     setByteArray marr oldLen (max 0 (newLen - oldLen)) (0 :: HalfWord)
     return $ MHWA marr
-
 
 unsafeFreezeWordArray :: PrimMonad m => MutableWordArray m -> m ByteArray
 unsafeFreezeWordArray !(MWA marr) = unsafeFreezeByteArray marr
@@ -48,7 +56,6 @@ indexWordArray = indexByteArray
 indexHalfWordArray :: ByteArray -> Int -> HalfWord
 indexHalfWordArray = indexByteArray
 
-
 indexWordArrayM :: Monad m => ByteArray -> Int -> m FullWord
 indexWordArrayM !arr !i = return $ indexByteArray arr i
 
@@ -60,3 +67,11 @@ writeWordArray !(MWA marr) = writeByteArray marr
 
 writeHalfWordArray :: PrimMonad m => MutableHalfWordArray m -> Int -> HalfWord -> m ()
 writeHalfWordArray !(MHWA marr) = writeByteArray marr
+
+setWordArray :: PrimMonad m => MutableWordArray m -> Int -> Int -> FullWord -> m ()
+setWordArray !(MWA marr) !off !count !word = setByteArray marr off count word
+
+copyWordArray :: PrimMonad m => MutableWordArray m -> Int -> ByteArray -> Int -> Int -> m ()
+copyWordArray !(MWA marr) !doff !arr !soff !wrds =
+    let !wordsize = sizeOf (0 :: FullWord)
+    in copyByteArray marr (doff * wordsize) arr (soff * wordsize) (wrds * wordsize)
