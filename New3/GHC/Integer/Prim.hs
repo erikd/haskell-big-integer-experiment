d35048dc6f2b89e4972f799a5e430e2b6c2b454c
Erik de Castro Lopo <erikd@mega-nerd.com>
2014-01-26 09:15:49 +1100

    New3/.../Prim.hs : Add plusWord2.

--- New3/GHC/Integer/Prim.hs
+++ New3/GHC/Integer/Prim.hs
@@ -3,7 +3,7 @@
 #include "MachDeps.h"
 
 module New3.GHC.Integer.Prim
-    ( plusWord2, plusWord2C, plusWord3C
+    ( plusWord, plusWord2, plusWord2C, plusWord3C
     , minusWord2, minusWord2C
     , timesWord2, timesWord2C, timesWord2CC
     , shiftRWord
@@ -14,26 +14,32 @@ import GHC.Types (isTrue#)
 import GHC.Prim
 
 
+{-# INLINE plusWord #-}
+plusWord :: Word -> Word -> Word
+plusWord !(W# a) !(W# b) =
+    let !s = plusWord# a b
+    in W# s
+
 {-# INLINE plusWord2 #-}
 plusWord2 :: Word -> Word -> (# Word, Word #)
 plusWord2 !(W# a) !(W# b) =
-    let (# c, s #) = plusWord2# a b
+    let (# !c, !s #) = plusWord2# a b
     in (# W# c, W# s #)
 
 {-# INLINE plusWord2C #-}
 plusWord2C :: Word -> Word -> Word -> (# Word, Word #)
 plusWord2C !(W# a) !(W# b) !(W# c) =
-    let (# c1, s1 #) = plusWord2# a b
-        (# c2, s2 #) = plusWord2# s1 c
+    let (# !c1, !s1 #) = plusWord2# a b
+        (# !c2, !s2 #) = plusWord2# s1 c
         !carry = plusWord# c1 c2
     in (# W# carry, W# s2 #)
 
 {-# INLINE plusWord3C #-}
 plusWord3C :: Word -> Word -> Word -> Word -> (# Word, Word #)
 plusWord3C !(W# a) !(W# b) !(W# c) !(W# d) =
-    let (# c1, s1 #) = plusWord2# a b
-        (# c2, s2 #) = plusWord2# c d
-        (# c3, s3 #) = plusWord2# s1 s2
+    let (# !c1, !s1 #) = plusWord2# a b
+        (# !c2, !s2 #) = plusWord2# c d
+        (# !c3, !s3 #) = plusWord2# s1 s2
         !c4 = plusWord# c1 c2
         !carry = plusWord# c3 c4
     in (# W# carry, W# s3 #)
@@ -56,24 +62,24 @@ minusWord2C !(W# a) !(W# b) !(W# c) =
 {-# INLINE timesWord2 #-}
 timesWord2 :: Word -> Word -> (# Word, Word #)
 timesWord2 !(W# a) !(W# b) =
-    let (# ovf, prod #) = timesWord2# a b
+    let (# !ovf, !prod #) = timesWord2# a b
     in (# W# ovf, W# prod #)
 
 {-# INLINE timesWord2C #-}
 timesWord2C :: Word -> Word -> Word -> (# Word, Word #)
 timesWord2C !(W# a) !(W# b) !(W# c) =
-    let (# ovf, prod #) = timesWord2# a b
-        (# cry, prodc #) = plusWord2# prod c
-        carry = plusWord# ovf cry
+    let (# !ovf, !prod #) = timesWord2# a b
+        (# !cry, !prodc #) = plusWord2# prod c
+        !carry = plusWord# ovf cry
     in (# W# carry, W# prodc #)
 
 {-# INLINE timesWord2CC #-}
 timesWord2CC :: Word -> Word -> Word -> Word -> (# Word, Word #)
 timesWord2CC !(W# a) !(W# b) !(W# c) !(W# d) =
-    let (# ovf, prod #) = timesWord2# a b
-        (# c1, sm #) = plusWord2# c d
-        (# cry, prodc #) = plusWord2# prod sm
-        carry = plusWord# (plusWord# ovf cry) c1
+    let (# !ovf, !prod #) = timesWord2# a b
+        (# !c1, !sm #) = plusWord2# c d
+        (# !cry, !prodc #) = plusWord2# prod sm
+        !carry = plusWord# (plusWord# ovf cry) c1
     in (# W# carry, W# prodc #)
 
 {-# INLINE shiftRWord #-}
